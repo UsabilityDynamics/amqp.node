974de4719738bd01756a4cb63ca158364bbdbc19
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-04-13 20:06:22 -0400

    added extended options

---
+++ examples/extended-options.js
@@ -0,0 +1,27 @@
+/**
+ * Extended Options Example
+ *
+ * Example with extended options AMQP client options being set.
+ *
+ * @author potanin@UD
+ * @type {exports}
+ */
+var amqp  = require('../');
+var fs    = require('fs');
+
+var connection = amqp.connect( 'amqp://guest:qoszeubbegzwqlrj@localhost', {
+  port: 5672,
+  virtualHost: 'test',
+  clientProperties: {
+    product: 'test-product',
+    version: '999',
+    platform: 'test-platform',
+    information: 'test-information',
+    capabilities: {
+      my_cap: true
+    }
+  }
+});
+
+
+
